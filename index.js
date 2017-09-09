b9480ec43957f75edfdf9b06a5ac74d741a2ad5e
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-09-09 19:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+  var price = Math.floor(Math.random()*100);
+  cart.push({[item]:price})
+  console.log(`${item} has been added to your cart.`)
+  return cart;
  // write your code here
 }
 
 function viewCart() {
-  // write your code here
+  var cartStr = 'In your cart, you have ';
+  if (cart.length === 0){
+    return `Your shopping cart is empty.`;
+  }
+  else {
+    for (var prop in cart){
+      cartStr += `${prop} at ${obj[prop]}`
+    }
+  }
+  return cartStr;
 }
 
 function total() {
+  var total =0;
+  for (var n = 0; n<cart.length; n++){
+    total += cart[n][1].parseInt();
+  }
+  return total;
   // write your code here
 }
 
